ddc5ef3ec1cd5787e014efa36d98e0378329ed1a
viverbungag <viverbungag1@gmail.com>
2021-08-07 23:31:39 +0800

    add query

---
+++ MainQuery.sql
@@ -0,0 +1,11 @@
+USE im;
+
+CREATE TABLE students (
+		student_id int,
+        student_name text,
+        student_address text,
+        student_course text,
+        student_gender text,
+        student_year text
+	);
+    
